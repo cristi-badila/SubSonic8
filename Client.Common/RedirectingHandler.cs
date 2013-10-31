f3262ad6bd646ebf23af61a7cb1ee1f033fc2e34
cristibadila <Cristi.Badila@gmail.com>
2013-10-31 13:38:25 +0200

    updates the service result base to allow http compression and passing along the authorization header on authentication

---
+++ Client.Common/RedirectingHandler.cs
@@ -0,0 +1,74 @@
+﻿namespace Client.Common
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class RedirectingHandler : HttpClientHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                response = await base.SendAsync(request, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.ServiceUnavailable) { ReasonPhrase = e.Message };
+            }
+
+            return IsRedirect(response) ? await base.SendAsync(GetRequestForRedirect(response), cancellationToken) : response;
+        }
+
+        private static bool IsRedirect(HttpResponseMessage response)
+        {
+            return response.StatusCode == HttpStatusCode.MovedPermanently
+                   || response.StatusCode == HttpStatusCode.Moved
+                   || response.StatusCode == HttpStatusCode.Redirect
+                   || response.StatusCode == HttpStatusCode.Found
+                   || response.StatusCode == HttpStatusCode.SeeOther
+                   || response.StatusCode == HttpStatusCode.RedirectKeepVerb
+                   || response.StatusCode == HttpStatusCode.TemporaryRedirect
+                   || (int)response.StatusCode == 308;
+        }
+
+        private static HttpRequestMessage GetRequestForRedirect(HttpResponseMessage response)
+        {
+            var newRequest = CopyRequest(response.RequestMessage);
+            if (response.StatusCode == HttpStatusCode.Redirect
+                || response.StatusCode == HttpStatusCode.Found
+                || response.StatusCode == HttpStatusCode.SeeOther)
+            {
+                newRequest.Content = null;
+                newRequest.Method = HttpMethod.Get;
+            }
+
+            newRequest.RequestUri = response.Headers.Location;
+            return newRequest;
+        }
+
+        private static HttpRequestMessage CopyRequest(HttpRequestMessage oldRequest)
+        {
+            var newRequest = new HttpRequestMessage(oldRequest.Method, oldRequest.RequestUri);
+            foreach (var header in oldRequest.Headers)
+            {
+                newRequest.Headers.TryAddWithoutValidation(header.Key, header.Value);
+            }
+
+            foreach (var property in oldRequest.Properties)
+            {
+                newRequest.Properties.Add(property);
+            }
+
+            if (oldRequest.Content != null)
+            {
+                newRequest.Content = new StreamContent(oldRequest.Content.ReadAsStreamAsync().Result);
+            }
+
+            return newRequest;
+        }
+    }
+}
--- Client.Common/Results/ServiceResultBase.cs
+++ Client.Common/Results/ServiceResultBase.cs
@@ -1,12 +1,20 @@
 ﻿namespace Client.Common.Results
 {
+    using System.Net;
     using System.Net.Http;
     using System.Xml.Linq;
     using Client.Common.Services.DataStructures.SubsonicService;
+    using global::Common.Exceptions;
     using global::Common.Results;
 
     public abstract class ServiceResultBase<T> : RemoteXmlResultBase<T>, IServiceResultBase<T>
     {
+            new HttpClient(new RedirectingHandler
+            {
+	        AllowAutoRedirect = true,
+                PreAuthenticate = true,
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+            });
         protected readonly XNamespace Namespace = "http://subsonic.org/restapi";
 
         public override string RequestUrl
